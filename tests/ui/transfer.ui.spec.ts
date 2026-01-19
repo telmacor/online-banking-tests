fca79f56a2c46d0b811ae2068d4854819a69dd9f
Telma Correia <telmacorreia@Telmas-Air.Home>
2026-01-19 20:31:26 +0000

    Add ui tests

--- tests/ui/transfer.ui.spec.ts
+++ tests/ui/transfer.ui.spec.ts
@@ -27,15 +27,102 @@ test.describe('Transfer Form', () => {
         await expect(page.locator('#confirmationMessage')).toContainText('Transfer Successful!');
     });
 
-    test('Form validation for empty/invalid fields', async ({ page }) => {
+    test.describe('Form errors validation', () => {
+    
+        test('From account field validations', async ({ page }) => {
+            const transferPage = new TransferPage(page);
+            await transferPage.open();
 
-    });
+            const fromAccount = page.locator('#fromAccount');
+            const form: TransferForm = {
+                from_account: '',
+                to_account: 'BBB',
+                amount: 100,
+                currency: 'EUR'
+            };
 
-    test('Validation for same account transfer attempt', async ({ page }) => {
+            await transferPage.makeTransfer(form);
+            // Validate message when empty 
+            await expect(fromAccount).toHaveJSProperty('validationMessage', 'Please fill out this field.');
 
-    });
+            const fromAccountError = page.locator('#fromAccountError');
+            await page.fill('#fromAccount', 'AA');
+            await page.click('#transferButton');
 
-    test('Insufficient funds error message display', async ({ page }) => {
+            //Validate message when less that 3 characters
+            await expect(fromAccountError).toHaveText("Account ID must be at least 3 characters")
+        });
 
+        test('To account field validations', async ({ page }) => {
+            const transferPage = new TransferPage(page);
+            await transferPage.open();
+
+            const toAccount = page.locator('#toAccount');
+            const form: TransferForm = {
+                from_account: 'AAA',
+                to_account: '',
+                amount: 100,
+                currency: 'EUR'
+            };
+
+            await transferPage.makeTransfer(form);
+
+            // From Account should have the validation message
+            await expect(toAccount).toHaveJSProperty('validationMessage', 'Please fill out this field.');
+        });
+
+        test('Amount field validations', async ({ page }) => {
+            const transferPage = new TransferPage(page);
+            await transferPage.open();
+
+            const amount = page.locator('#amount');
+            const form: TransferForm = {
+                from_account: 'AAA',
+                to_account: 'BBB',
+                amount: 0,
+                currency: 'EUR'
+            };
+
+            await transferPage.makeTransfer(form);
+
+            // From Account should have the validation message
+            await expect(amount).toHaveJSProperty('validity.rangeUnderflow', true);
+        });
+
+        test('Validation for same account transfer attempt', async ({ page }) => {
+            const transferPage = new TransferPage(page);
+            await transferPage.open();
+
+            const toAccount = page.locator('#toAccount');
+            const form: TransferForm = {
+                from_account: 'AAA',
+                to_account: 'AAA',
+                amount: 100,
+                currency: 'EUR'
+            };
+
+            await transferPage.makeTransfer(form);
+
+            const toAccountError = page.locator('#toAccountError');
+            await expect(toAccountError).toHaveText("Cannot transfer to the same account")
+        });
+
+        test('Insufficient funds error message display', async ({ page }) => {
+            const transferPage = new TransferPage(page);
+            await transferPage.open();
+
+            const toAccount = page.locator('#toAccount');
+            const form: TransferForm = {
+                from_account: 'AAA',
+                to_account: 'BBB',
+                amount: 1500.76,
+                currency: 'USD'
+            };
+
+            await transferPage.makeTransfer(form);
+
+            const amountError = page.locator('#amountError');
+            await expect(amountError).toHaveText("Insufficient funds")
+        });
     });
 });
