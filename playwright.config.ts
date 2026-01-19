bab48ee44a34c3041810a0b1d76292519323b7b2
Telma Correia <telmacorreia@Telmas-Air.Home>
2026-01-19 21:15:44 +0000

    Add playwright reports

--- playwright.config.ts
+++ playwright.config.ts
@@ -27,7 +27,8 @@ export default defineConfig({
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: [
-    ['list'], // console
+    ['html'],
+    ['list'], 
     ['allure-playwright'] // Allure
   ],
   use: {
